1cfdec5b6b4346c10dc40974d41f437e6bb8efc3
Richard Campbell <betam4x@gmail.com>
2013-12-02 14:20:48 -0500

    Oops, secret token was in gitignore.  Not necessary for example app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoogleAuthExample::Application.config.secret_key_base = '00fa4dd6b29566a84f861f09d584e3bb537503f11cff82d56e7f533de2bdcd40064362bf150cad2130a114dedbc407b5c7f465e47d0a5b9d82d38dd9bed3534b'
