b827be51506e41e9b9a0c6f36ce9242913732f95
Richard Campbell <rich@richonrails.com>
2016-06-08 17:29:40 -0500

    Fix forbidden attributes error.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   def self.from_omniauth(auth)
-    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
